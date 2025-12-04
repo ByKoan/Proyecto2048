297da0351c6c5c3e07aac21fe508a4f425ae3803
cetoprca <cetoprca@gmail.com>
2025-12-04 11:09:40 +0100

    Añadida interfaz basica para el juego

--- app/src/main/java/com/dam2/proyecto2048/vistas/Game.kt
+++ app/src/main/java/com/dam2/proyecto2048/vistas/Game.kt
@@ -1,10 +1,16 @@
 package com.dam2.proyecto2048.vistas
 
+import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
+import android.view.Gravity
+import android.widget.GridLayout
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.dam2.proyecto2048.R
 import com.dam2.proyecto2048.databinding.GameBinding
 
 class Game : AppCompatActivity() {
@@ -24,7 +30,7 @@ class Game : AppCompatActivity() {
         }
 
         buttonBehavior()
-
+        createBoard()
     }
 
     private fun buttonBehavior() {
@@ -32,6 +38,43 @@ class Game : AppCompatActivity() {
 
         salirButton.setOnClickListener {
             finish()
+        }
+    }
+
+    private fun createBoard(){
+        var tablero = gameBinding.tablero;
+
+        var lista2d = MutableList(4){ MutableList(4){""} }
+
+        for (i in 0 until 4) {
+            for (j in 0 until 4) {
+                lista2d[i][j] = "$i $j"
+            }
+        }
+
+        Log.i("GAME", "${lista2d}")
+
+        tablero.removeAllViews() // Limpiar si es necesario
+
+        tablero.columnCount = 4
+        tablero.rowCount = 4
+
+        for (i in 0 until lista2d.size) {
+            for (j in 0 until lista2d[i].size) {
+                val textView = TextView(this).apply {
+                    text = lista2d[i][j]
+                    gravity = Gravity.CENTER
+                    height = 160
+                    width = 160
+                    setBackgroundResource(R.drawable.cell_border)
+                    layoutParams = GridLayout.LayoutParams().apply {
+                        columnSpec = GridLayout.spec(j, 1f)
+                        rowSpec = GridLayout.spec(i, 1f)
+                    }
+                }
+
+                tablero.addView(textView)
+            }
         }
     }
 }
